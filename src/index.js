8a20b67d2179296597ecaa9532123a74b793db5c
Justin Goats <justingoats@yahoo.com>
2021-12-02 19:53:50 -0800

    started a React application with a navigation , hero page , and a canvas component that will have a 3D animation to help engage the viewer

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from "react";
 import ReactDom from "react-dom";
 import Nav from "../src/components/navigation/nav.js";
 import Hero from "../src/components/hero-section/hero.js";
+import Canvas from "../src/components/canvas/canvas.js"
 import "tailwindcss/tailwind.css"
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                <Nav />
-                <Hero />
+                <div className="absolute">
+                    <Nav />
+                    <Hero />
+                </div>
+                <Canvas />
             </div>
         )
     }
