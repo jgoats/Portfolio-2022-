bc4060c1df990e1d2585ef0d00cd23d5f2e6cae4
Justin Goats <justingoats@yahoo.com>
2021-12-02 20:57:41 -0800

    added the Poppins custom font inside tailwind

--- src/index.js
+++ src/index.js
@@ -2,19 +2,16 @@ import React from "react";
 import ReactDom from "react-dom";
 import Nav from "../src/components/navigation/nav.js";
 import Hero from "../src/components/hero-section/hero.js";
-import Canvas from "../src/components/canvas/canvas.js"
 import "tailwindcss/tailwind.css"
 
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <div className="absolute">
-                    <Nav />
-                    <Hero />
-                </div>
-                <Canvas />
+            <div className="font-Poppins">
+                <Nav />
+                <Hero />
             </div>
+
         )
     }
 }
