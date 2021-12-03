6bdddc786d7754c6a06da2bc6655a399408e882f
Justin Goats <justingoats@yahoo.com>
2021-12-02 19:52:25 -0800

    added a classes.js file to take notes on tailwind.css

---
+++ src/classes.js
@@ -0,0 +1,95 @@
+// tailwind classes..
+
+/*
+bg-{color}-{shade}
+
+1 rem = 16px = 4 in tailwind;
+you can use from 1 - 64 in tailwind;
+w-4 = 16px width in tailwind; (multiplied by 4)
+h-4 = 16px height in tailwind; (multiplied by 4)
+w-1/4 = 1/4 the width based on the elements container;
+w-screen = entire width of viewport;
+w-full = 100% width of container element
+
+padding and margin are also similar...
+p-4 = padding of 16px;
+m-4 = margin of 16px;
+pl-4 = padding of 16px on the left side
+ml-4 = margin of 16px on the left side
+px-4 = padding  of 16px on left and right side
+py-4 = padding of 16px on the top and bottom
+
+text-size uses a Sizes , rem , and pixels for values
+text-base = 16px or 1 rem
+text-lg = 18px
+text-xl = 20px
+text-2xl = 24px
+
+aligning text...
+text-left = left
+text-right = right
+text-center = center
+text-justify = wraps text evenly
+
+styling text
+text-color-shade
+text-blue-500
+
+italic or not italic
+italic...
+not-italic
+
+font-{weight} 100 - 900 or specific names like normal semibold bold or black
+
+letter spacing is described as tracking and is measured in em not rem. em is based on the parent font size.
+tracking-normal , tracking-wide , wider widest
+
+line-height is expressed as leading...
+leading- none tight snug normal relaxed loose
+
+text-decoration
+underline , no-underline , line-through
+
+text-transform
+uppercase lowercase capitalize normal-case
+
+borders...
+border-2 , 4 , 8
+writing just border gives you a 1px light grey border
+
+border-side-width... t , b , l , r
+border-t-2 gives a top border of 2 pixels
+
+border-color-shade
+border-blue-400
+
+border-style
+border-dotted , border-dashed , border-solid , border-double border-none
+
+border-radius is expressed as rounded
+rounded-sm , lg , full , none , or just rounded for 2px rounded corners
+
+rounded-side-radius
+t,l,b,r,tl,tr,bl,br
+
+center elements horizontally
+//parent element flex justify-center
+flex-wrap if multiple elements
+
+flex simply creates a flex container...
+
+flexbox direction vertical
+items-center will center items vertically
+
+flex-col will set the flex items into a column instead of a row format...
+
+
+
+
+
+
+
+
+
+*/
+
