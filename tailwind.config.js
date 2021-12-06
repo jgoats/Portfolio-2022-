834ef5ed5cc94fa799a4a5ef299caaf4402fb3b2
Justin Goats <justingoats@yahoo.com>
2021-12-05 22:03:18 -0800

    added  btn , btn-primary , and btn-secondary classes to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "media", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bookmark-purple": "#5267DF",
+        "bookmark-red": "#FA5959",
+        "bookmark-blue": "#242A45",
+        "bookmark-grey": "#9194A2",
+        "bookmark-white": "#f7f7f7",
+
+      }
+    },
     fontFamily: {
       Poppins: ["Poppins", "sans-serif"]
     },
