a1edf4f0d21757ee599704e8861a0eba9e3b62eb
Justin Goats <justingoats@yahoo.com>
2021-12-05 22:03:54 -0800

    created a hero section for my portfolio website

--- src/components/hero-section/hero.js
+++ src/components/hero-section/hero.js
@@ -1,11 +1,13 @@
 import React from 'react';
-//import styles from "./hero.css";
 import "../../../dist/css/styles.css";
 
 export default function Hero() {
+
     return (
-        <div className="bg-red-500">
-            this is red
-        </div>
+        <main className="text-center flex flex-col flex-1 container">
+            <h1 className=" dark:text-white z-10 text-gray-800 text-3xl mb-4">Hi, My Name Is Justin And I Am A Web Developer</h1>
+            <p className=" dark:text-white z-10 text-gray-800 mb-8">Click below to view some of my work</p>
+            <div className="hover:text-black z-10"><button className=" hover:bg-white hover:text-black transition duration-300 z-10 btn btn-primary">View Projects</button></div>
+        </main>
     )
 }
