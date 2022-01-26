adf1daf2d5438967c99bdeb1291e01a05a962fb0
Justin Goats <justingoats@yahoo.com>
2022-01-26 12:17:09 -0800

    incorporated an intersection observer api to check for the position of the navigation and change the style when the user scrolls down the page.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
 import React from "react";
 import ReactDom from "react-dom";
-import Nav from "../src/components/navigation/nav.js";
-import Hero from "../src/components/hero-section/hero.js";
-import "tailwindcss/tailwind.css"
+import Hero from "./components/hero/hero.js";
+import Nav from "../src/components/nav/nav.js";
+import Hamburger from "./components/hamburger/hamburger.js";
+import styles from "tailwindcss/tailwind.css";
+import AboutMe from "../src/components/aboutme/aboutme.js";
+import Projects from "../src/components/projects/projects.js";
+import "../dist/css/styles.css";
+import "./index.css";
 
-class App extends React.Component {
+export default class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            nav: ["nav", "nav-item", "hamburger-item-white"]
+        }
+        this.intersectionCallback = this.intersectionCallback.bind(this);
+    }
+    intersectionCallback(entries) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                this.setState({
+                    nav: ["animate-nav", "nav-item-animate", "hamburger-item-black"]
+                })
+            }
+            else {
+                this.setState({
+                    nav: ["nav", "nav-item", "hamburger-item-white"]
+                })
+            }
+        });
+    }
+
     render() {
         return (
             <div className="font-Poppins">
-                <Nav />
                 <Hero />
+                <Nav nav={this.state.nav} />
+                <Hamburger nav={this.state.nav} />
+                <div className="scroll-object absolute w-full h-full">
+                    <AboutMe intersectionCallback={this.intersectionCallback} />
+                    <Projects intersectionCallback={this.intersectionCallback} />
+                </div>
             </div>
 
         )
