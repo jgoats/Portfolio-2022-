3b349925c77f76ab4c2246774f32d7a958e354a4
Justin Goats <justingoats@yahoo.com>
2022-02-01 23:27:44 -0800

    removed the blog link from the navigation component

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -5,12 +5,11 @@ import "./nav.css";
 export default class Nav extends React.Component {
     render() {
         return (
-            <header className={`${this.props.nav[0]} fixed z-10 top-0 flex w-full`}>
+            <header className={`${this.props.nav[0]} fixed z-20 top-0 flex w-full`}>
                 <div className="flex justify-end items-center w-full">
                     <ul className="nav-container md:flex hidden items-center justify-end flex-1 gap-20">
                         <li className={`${this.props.nav[1]}`}>about me</li>
                         <li className={`${this.props.nav[1]}`}>projects</li>
-                        <li className={`${this.props.nav[1]}`}>blog</li>
                         <li className={`${this.props.nav[1]}`}>contact me</li>
                     </ul>
                 </div>
